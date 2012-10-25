f010b42ca03c65af0ff5a238edf9fa1281740a47
Nick Devereaux <nickjdevereaux@gmail.com>
2012-10-25 12:12:47 +1100

    fixed form encoding and support for page move

--- instapaper2kindle.py
+++ instapaper2kindle.py
@@ -2,11 +2,12 @@
 # -*- coding: UTF-8 -*-
 import os
 import sys
+#import pdb
 import cookielib, urllib, urllib2, socket, httplib
 import ConfigParser
 
 
-VERSION = '0.1'
+VERSION = '0.2'
 print '\nkindle2instapaper v%s'%VERSION
 print 'written by Nick Devereaux (nickjdevereaux',
 print '@ gmail.com)\n'
@@ -15,6 +16,7 @@ loginurl = 'http://www.instapaper.com/user/login'
 sendtokindleurl = 'http://www.instapaper.com/user/kindle_send_now'
 kindleurl = 'http://www.instapaper.com/user/kindle'
 cookiename = "instapaper.cookie"
+
 
 try:
 	from BeautifulSoup import BeautifulSoup
@@ -43,35 +45,46 @@ def sendtokindle():
 		print " Please set a username in settings.cfg file"
 		quit()
 	http_args = urllib.urlencode(dict(username=username, password=password))
+	config = {'verbose':sys.stderr}
+	h = {'User-Agent':'instapaper2kindle by nickjdevereaux'}
+	s = requests.session(headers=h,config=config)
+	#pdb.set_trace()
 	print 'Logging in with username %s...'%username,
-	r = requests.post(loginurl,data=http_args)
-	if(r.cookies is None):
+	formheaders = {'content-type':'application/x-www-form-urlencoded'}
+	r = s.post(loginurl,headers=formheaders,data=http_args)
+	c = r.cookies
+	if(c is None or len(c) == 0):
 		print 'failed.'
 		print 'ERROR: Failed to login. Please check your settings'
 		quit()
-	print 'success.'
-	c = r.cookies
+	print 'success. (%s)'%r.status_code
 	#get page check if we are logged in
-	kindlepage = requests.get(kindleurl,cookies=c)
+	kindlepage = s.get(kindleurl)
+	print 'fetched kindle page (%s)'%kindlepage.status_code
 	http_args = urllib.urlencode(dict(form_key=getformkey(kindlepage.content)))
 	print 'Submitting form...',
-	formresult = requests.post(sendtokindleurl,data=http_args, cookies=c)
+	formresult = s.post(sendtokindleurl,headers=formheaders,data=http_args)
 	if(formresult.status_code == 200):
 		print 'Success! =D'
 	else:
 		print 'Failed =('
+		print formresult.status_code
 
 def readconfig():
 	config = ConfigParser.RawConfigParser()
-	config.read(os.path.join(sys.path[0],'settings.cfg'))
+	path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'settings.cfg')
+	print 'reading from config file %s '%path
+	config.read(path)
 	global username
 	global password
+	#global lastrun
 	username = config.get('credentials','username') 
 	password = config.get('credentials','password') 
+	#lastrun = config.get('stats','lastrun')
 
 def getformkey(content):
 	soup = BeautifulSoup(content)
-	search = soup.findAll('input',attrs={'id':'form_key_send_now'})
+	search = soup.findAll('input',attrs={'id':'form_key'})
 	if(len(search) != 1):
 		print 'ERROR: Page structure changed, couldn\'t find form key'
 		print 'Please email me at nickjdevereaux',
